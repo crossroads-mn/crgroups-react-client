4f1db3dd224e1a2759ef814c5bc605656ec6639d
Benjamin Spain <benjamin@thespains.net>
2020-09-05 14:10:00 -0500

    Group details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -20,7 +20,7 @@ const Transition = React.forwardRef(function Transition(props: any, ref: any) {
 
 const App = (props: AppProps) => {
     const [open, setOpen] = React.useState(false);
-    const [group, setGroup] = React.useState(props.data[0])
+    const [group, setGroup] = React.useState(props.data[0]);
 
     const handleGroupClick = (group: GroupData) => {
         console.log(`Group clicked: ${group.TITLE}`);
@@ -43,16 +43,59 @@ const App = (props: AppProps) => {
                 { listItems }
             </List>
             <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition as any}>
-                <AppBar>
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                            {group.TITLE}
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                <Toolbar>
+                    <Typography variant="h5">
+                        {group.TITLE}
+                    </Typography>
+                    <Button>Sign Up</Button>
+                    <IconButton edge="end" color="inherit" onClick={handleClose} aria-label="close">
+                        <CloseIcon />
+                    </IconButton>
+                </Toolbar>
+                <div style={{ margin: '4px' }}>
+                    <Typography variant="h6">
+                        Description
+                    </Typography>
+                    <Typography variant="body1">
+                        {group.DESCRIPTION}
+                    </Typography>
+                    <span>
+                        <Typography variant="h6">Who Should Attend?</Typography>
+                        <Typography variant="body1">{group.TARGET_AUDIENCE}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Day the Group Meets?</Typography>
+                        <Typography variant="body1">{group.MEET_DAY}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Number of Weeks?</Typography>
+                        <Typography variant="body1">{group.DURATION}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Time Group Starts and Ends?</Typography>
+                        <Typography variant="body1">{group.MEET_TIME_START}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Leader's Name</Typography>
+                        <Typography variant="body1">{group.LEADER}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Email Address</Typography>
+                        <Typography variant="body1">{group.EMAIL}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Location Where Group Meets?</Typography>
+                        <Typography variant="body1">{group.LOCATION}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Cost</Typography>
+                        <Typography variant="body1">{group.COST}</Typography>
+                    </span>
+                    <span>
+                        <Typography variant="h6">Is Childcare Provided?</Typography>
+                        <Typography variant="body1">{ (group.CARE_PROVIDED == '1') ? "Yes" : "No" }</Typography>
+                    </span>
+                </div>
             </Dialog>
         </>
     )
--- client/src/models/GroupData.ts
+++ client/src/models/GroupData.ts
@@ -1,5 +1,19 @@
 export interface GroupData {
-    TITLE: string
+    ACTIVE: '0' | '1',
+    CAMPUS: 'Woodbury' | 'Eagan' | 'Cottage Grove' | 'Hastings' | 'Other',
+    CARE_PROVIDED: '0' | '1',
+    CATEGORY: 'Young Adult' | 'Men' | 'Women' | 'Couples' | 'Interest' | 'Support',
+    COST: string,
     DESCRIPTION: string,
-    GROUP_TYPE: 'Zoom' | 'Outdoor' | 'Inside'
+    DURATION: string,
+    EMAIL: string,
+    GROUP_LINK: string,
+    GROUP_TYPE: 'Zoom' | 'Outdoor' | 'Inside',
+    LEADER: string,
+    LOCATION: string,
+    MEET_DAY: 'Sunday' | 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday',
+    MEET_TIME_START: string,
+    PHONE_NUMBER: string,
+    TARGET_AUDIENCE: string,
+    TITLE: string
 }
